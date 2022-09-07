a6a87b86ea04b06e54398c58a4ae6893bc7843ae
dodeon101 <dodeon@101.inc>
2022-09-08 06:40:34 +0900

    데이터 접근 핵심 원리: JDBC 개발 - 조회

--- src/main/java/hello/kimyounghanjdbc/repository/MemberRepositoryV0.java
+++ src/main/java/hello/kimyounghanjdbc/repository/MemberRepositoryV0.java
@@ -5,12 +5,46 @@ import hello.kimyounghanjdbc.domain.Member;
 import lombok.extern.slf4j.Slf4j;
 
 import java.sql.*;
+import java.util.NoSuchElementException;
 
 /**
  * JDBC - DriverManager 사용
  */
 @Slf4j
 public class MemberRepositoryV0 {
+
+    public Member findById(String memberId) throws SQLException {
+        String sql = "select * from member where member_id = ?";
+
+        Connection con = null;
+        PreparedStatement pstmt = null;
+        ResultSet rs = null;
+
+        try {
+            con = getConnection();
+            pstmt = con.prepareStatement(sql);
+
+            pstmt.setString(1, memberId);
+            rs = pstmt.executeQuery();
+
+            if (rs.next()) {
+                Member member = new Member();
+                member.setMemberId(rs.getString("member_id"));
+                member.setMoney(rs.getInt("money"));
+
+                return member;
+            } else {
+                throw new NoSuchElementException("member not found memberId=" + memberId);
+            }
+        } catch (SQLException e) {
+            log.error("db error", e);
+            throw e;
+        } finally {
+            close(con, pstmt, rs);
+        }
+    }
+
+
     public Member save(Member member) throws SQLException {
         String sql = "insert into member(member_id, money) values(?, ?)";
         Connection con = null;
