34866092d23259ee54efa82b0a3da8e5557fc312
sojeongw <sojeongw.official@gmail.com>
2025-02-15 20:24:26 +0900

    트랜잭션 문제 해결

---
+++ src/main/java/hello/kimyounghanjdbc/service/MemberServiceV3_1.java
@@ -0,0 +1,55 @@
+package hello.kimyounghanjdbc.service;
+
+import hello.kimyounghanjdbc.domain.Member;
+import hello.kimyounghanjdbc.repository.MemberRepositoryV2;
+import hello.kimyounghanjdbc.repository.MemberRepositoryV3;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.TransactionStatus;
+import org.springframework.transaction.support.DefaultTransactionDefinition;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.SQLException;
+
+/*
+ * 트랜잭션 - 파라미터 연동, 풀을 고려한 종료
+ *  */
+@Slf4j
+@RequiredArgsConstructor
+public class MemberServiceV3_1 {
+
+    private final PlatformTransactionManager transactionManager;
+    private final MemberRepositoryV3 memberRepository;
+
+    public void accountTransfer(String fromId, String toId, int money) throws SQLException {
+        // 트랜잭션 시작
+        TransactionStatus status = transactionManager.getTransaction(new DefaultTransactionDefinition());
+        try {
+            // 비즈니스 로직 수행
+            bizLogic(fromId, toId, money);
+
+            // 커밋
+            transactionManager.commit(status);
+        } catch (Exception e) {
+            transactionManager.rollback(status);
+            throw new IllegalStateException(e);
+        }
+    }
+
+    private void bizLogic(String fromId, String toId, int money) throws SQLException {
+        Member fromMember = memberRepository.findById(fromId);
+        Member toMember = memberRepository.findById(toId);
+
+        memberRepository.update(fromId, fromMember.getMoney() - money);
+        validation(toMember);
+        memberRepository.update(toId, toMember.getMoney() + money);
+    }
+
+    private void validation(Member toMember) {
+        if (toMember.getMemberId().equals("ex")) {
+            throw new IllegalStateException("이체 중 예외 발생");
+        }
+    }
+}
